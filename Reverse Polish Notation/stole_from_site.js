5ff5c997db1e9824f929c8ccbf1932d56bf4e99f
Sergey Z <47861714+SamuraJey@users.noreply.github.com>
2023-05-21 15:08:34 +0500

    updated looks

--- Reverse Polish Notation/stole_from_site.js
+++ Reverse Polish Notation/stole_from_site.js
@@ -1,12 +1,14 @@
-function isOperator(c) {
+function isOperator(c)
+{
     return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
 }
 
-function leftAssoc(c) {
-    return c != '^';
-}
+// function leftAssoc(c) {
+//     return c != '^';
+// }
 
-function priority(c) {
+function priority(c)
+{
     if (c == '^') return 3;
     if (c == '*') return 2;
     if (c == '/') return 2;
@@ -15,7 +17,8 @@ function priority(c) {
     return 0;
 }
 
-function rightPriority(c) {
+function rightPriority(c)
+{
     if (c == '+') return 1;
     if (c == '-') return 2;
     if (c == '*') return 3;
@@ -24,79 +27,85 @@ function rightPriority(c) {
     return 0;
 }
 
-function infixToPostfix(expr) {
-    var i = 0,
-        nextToken = function () {
-            while (i < expr.length && expr[i] == ' ') i++;
-            if (i == expr.length) return '';
-            var b = '';
-            while (i < expr.length && expr[i] != ' ' && expr[i] != '(' && expr[i] != ')' && !isOperator(expr[i])) b += expr[i++];
-            if (b != '') return b;
-            return expr[i++];
-        };
-    var S = [],
-        O = [],
-        tok;
-    while ((tok = nextToken()) != '') {
-        if (tok == '(') S.push(tok);
-        else if (tok == ')') {
-            while (S.length > 0 && S[S.length - 1] != '(') O.push(S.pop());
-            if (S.length == 0) return 'Mismatched parenthesis.';
-            S.pop();
-        }
-        else if (isOperator(tok)) {
-            while (S.length > 0 && isOperator(S[S.length - 1]) && ((leftAssoc(tok) && priority(tok) <= priority(S[S.length - 1])) || (!leftAssoc(tok) && priority(tok) < priority(S[S.length - 1])))) O.push(S.pop());
-            S.push(tok);
-        }
-        else {
-            O.push(tok);
-        }
-    }
-    while (S.length > 0) {
-        if (!isOperator(S[S.length - 1])) return 'Mismatched parenthesis.';
-        O.push(S.pop());
-    }
-    if (O.length == 0) return 'Invalid expression.'
-    var s = '';
-    for (var i = 0; i < O.length; i++) {
-        if (i != 0) s += ' ';
-        s += O[i];
-    }
-    return s;
-}
+// function infixToPostfix(expr) {
+//     var i = 0,
+//         nextToken = function () {
+//             while (i < expr.length && expr[i] == ' ') i++;
+//             if (i == expr.length) return '';
+//             var b = '';
+//             while (i < expr.length && expr[i] != ' ' && expr[i] != '(' && expr[i] != ')' && !isOperator(expr[i])) b += expr[i++];
+//             if (b != '') return b;
+//             return expr[i++];
+//         };
+//     var S = [],
+//         O = [],
+//         tok;
+//     while ((tok = nextToken()) != '') {
+//         if (tok == '(') S.push(tok);
+//         else if (tok == ')') {
+//             while (S.length > 0 && S[S.length - 1] != '(') O.push(S.pop());
+//             if (S.length == 0) return 'Mismatched parenthesis.';
+//             S.pop();
+//         }
+//         else if (isOperator(tok)) {
+//             while (S.length > 0 && isOperator(S[S.length - 1]) && ((leftAssoc(tok) && priority(tok) <= priority(S[S.length - 1])) || (!leftAssoc(tok) && priority(tok) < priority(S[S.length - 1])))) O.push(S.pop());
+//             S.push(tok);
+//         }
+//         else {
+//             O.push(tok);
+//         }
+//     }
+//     while (S.length > 0) {
+//         if (!isOperator(S[S.length - 1])) return 'Mismatched parenthesis.';
+//         O.push(S.pop());
+//     }
+//     if (O.length == 0) return 'Invalid expression.'
+//     var s = '';
+//     for (var i = 0; i < O.length; i++) {
+//         if (i != 0) s += ' ';
+//         s += O[i];
+//     }
+//     return s;
+// }
 
-function postfixToInfix(expr) {
+function postfixToInfix(expr)
+{
     var i = 0,
-        nextToken = function () {
+        nextToken = function()
+        {
             while (i < expr.length && expr[i] == ' ') i++;
             if (i == expr.length) return '';
             var b = '';
             while (i < expr.length && expr[i] != ' ') b += expr[i++];
             return b;
         },
-        print = function (x) {
-            if (typeof (x) == 'string') return x;
+        print = function(x)
+        {
+            if (typeof(x) == 'string') return x;
             var l = print(x.l),
                 r = print(x.r);
-            if (typeof (x.l) != 'string' && (priority(x.l.op) < priority(x.op) || (x.l.op == x.op && x.op == '^')))
+            if (typeof(x.l) != 'string' && (priority(x.l.op) < priority(x.op) || (x.l.op == x.op && x.op == '^')))
                 l = '(' + l + ')';
-            if (typeof (x.r) != 'string' && (rightPriority(x.r.op) < rightPriority(x.op) || (x.l.op == x.op && (x.op == '-' || x.op == '/'))))
+            if (typeof(x.r) != 'string' && (rightPriority(x.r.op) < rightPriority(x.op) || (x.l.op == x.op && (x.op == '-' || x.op == '/'))))
                 r = '(' + r + ')';
             return l + ' ' + x.op + ' ' + r;
         };
     var S = [],
         tok;
-    while ((tok = nextToken(expr)) != '') {
-        if (isOperator(tok)) {
+    while ((tok = nextToken(expr)) != '')
+    {
+        if (isOperator(tok))
+        {
             if (S.length < 2) return 'Invalid expression.';
             S.push(
-                {
-                    op: tok,
-                    r: S.pop(),
-                    l: S.pop()
-                });
+            {
+                op: tok,
+                r: S.pop(),
+                l: S.pop()
+            });
         }
-        else {
+        else
+        {
             S.push(tok);
         }
     }
@@ -107,17 +116,78 @@ function postfixToInfix(expr) {
 let expr = "735/+1-85--";
 
 let spacedExpr = "";
-for (let i = 0; i < expr.length; i++) {
+for (let i = 0; i < expr.length; i++)
+{
     spacedExpr += expr[i];
-    if (i < expr.length - 1) {
+    if (i < expr.length - 1)
+    {
         spacedExpr += " ";
     }
 }
 console.log(spacedExpr);
 
-console.log(postfixToInfix(spacedExpr));
+console.log(postfixToInfix(expr));
 
 // var result = postfixToInfix("7 3 5 / + 1 - 8 5 - -");
 // console.log(result);
 
 // console.log(infixToPostfix("7+3/5-1-(8-5)"));
+
+function postfixToInfix5(expression)
+{
+
+    let spacedExpr = "";
+    for (let i = 0; i < expr.length; i++)
+    {
+        spacedExpr += expr[i];
+        if (i < expr.length - 1)
+        {
+            spacedExpr += " ";
+        }
+    }
+    expression = spacedExpr;
+
+    let i = 0;
+    const nextToken = function()
+    {
+        while (i < expression.length && expression[i] == ' ') i++;
+        if (i == expression.length) return '';
+        let token = '';
+        while (i < expression.length && expression[i] != ' ') token += expression[i++];
+        return token;
+    };
+    const printExpression = function(node)
+    {
+        if (typeof(node) == 'string') return node;
+        let left = printExpression(node.left),
+            right = printExpression(node.right);
+        if (typeof(node.left) != 'string' && (priority(node.left.op) < priority(node.op) || (node.left.op == node.op && node.op == '^')))
+            left = '(' + left + ')';
+        if (typeof(node.right) != 'string' && (rightPriority(node.right.op) < rightPriority(node.op) || (node.left.op == node.op && (node.op == '-' || node.op == '/'))))
+            right = '(' + right + ')';
+        return left + ' ' + node.op + ' ' + right;
+    };
+    const stack = [];
+    let token;
+    while ((token = nextToken(expression)) != '')
+    {
+        if (isOperator(token))
+        {
+            if (stack.length < 2) return 'Invalid expression.';
+            stack.push(
+            {
+                op: token,
+                right: stack.pop(),
+                left: stack.pop()
+            });
+        }
+        else
+        {
+            stack.push(token);
+        }
+    }
+    if (stack.length != 1) return 'Invalid expression.';
+    return printExpression(stack.pop()).replace(/\s/g, '');
+}
+
+console.log(postfixToInfix5(expr));
